ef9bf25fcc46c5ec692f2ac0c68751de3b9549aa
Tim Lu <victor.primoris@gmail.com>
2016-10-26 18:23:16 -0700

    Finished CSS styling round 1

--- myscript.js
+++ myscript.js
@@ -1,19 +1,22 @@
 jQuery(document).ready(function($) {
   var previousScroll = 0;
 
-    $(window).scroll(function(){
-       var currentScroll = $(this).scrollTop();
-       if (currentScroll > previousScroll){
-          $(".free-wrap").addClass("moving-down");
-       } else {
-          $(".free-wrap").addClass("moving-up");
-       }
-       previousScroll = currentScroll;
-       setTimeout(function(){
-        $(".free-wrap").removeClass("moving-up");
-          $(".free-wrap").removeClass("moving-down");
-       }, 250);
+  // Moves the freewrap as the user scrolls on the page
 
+    $(window).scroll(function(){
+      if ($("#free-wrap").length){
+        var currentScroll = $(this).scrollTop();
+         if (currentScroll > previousScroll){
+            $(".free-wrap").addClass("moving-down");
+         } else {
+            $(".free-wrap").addClass("moving-up");
+         }
+         previousScroll = currentScroll;
+         setTimeout(function(){
+          $(".free-wrap").removeClass("moving-up");
+            $(".free-wrap").removeClass("moving-down");
+         }, 250);
+      }
     });
 
   //Smoothly transitions button presses to the warranty block
@@ -44,6 +47,25 @@ jQuery(document).ready(function($) {
     });
   } else {
     $('#menu-item-201').animate({"opacity":"1"});
+  }
+
+
+  // Ensures previous sticky element fades in instead of just popping in
+
+  if($('.sticky-element').length){
+    var $lastFadeout;
+    $(window).scroll(function(){
+      var $currentSecondLast = $('.is_stucked').eq(-2);
+      if($('.is_stucked').length > 1 && $currentSecondLast !== $lastFadeout){
+        $('.is_stucked').removeClass("fadeout");
+        var $currentSecondLast = $('.is_stucked').eq(-2);
+        $prevFadeout = $currentSecondLast
+        $currentSecondLast.addClass("fadeout");
+      }
+      if($('.is_stucked').length === 1){
+        $('.is_stucked').removeClass("fadeout");
+      }
+    });
   }
 
 });
